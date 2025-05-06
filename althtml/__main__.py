4d5e9ad6f30bc33de2fd55cc2368c9a23314eda9
Varun Bhatnagar <bhatnagarvarun2020@gmail.com>
2025-05-06 12:56:33 -0400

    reload server

--- althtml/__main__.py
+++ althtml/__main__.py
@@ -2,6 +2,7 @@ import yaml
 from .watcher import run_watcher
 import argparse
 from pathlib import Path
+import time
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
                         prog='ProgramName',
@@ -11,7 +12,15 @@ if __name__ == '__main__':
     args = parser.parse_args()
     cfg = yaml.safe_load(open(args.config, "r"))
     base_path = Path('.')
-    no_write_paths = {header for header_str in cfg['headers'] for header in base_path.glob(header_str)} if 'headers' in cfg else set()
-    watch_paths = {watch_path for watch_path_str in cfg['watch'] for watch_path in base_path.glob(watch_path_str)} if 'watch' in cfg else set()
-    write_pairs = {Path(to_write['src']) : Path(to_write['dst']) for to_write in cfg['write']}
-    run_watcher(no_write_paths, write_pairs, watch_paths)
+    while True:
+        try:
+            no_write_paths = {header for header_str in cfg['headers'] for header in base_path.glob(header_str)} if 'headers' in cfg else set()
+            watch_paths = {watch_path for watch_path_str in cfg['watch'] for watch_path in base_path.glob(watch_path_str)} if 'watch' in cfg else set()
+            write_pairs = {Path(to_write['src']) : Path(to_write['dst']) for to_write in cfg['write']}
+            run_watcher(no_write_paths, write_pairs, watch_paths)
+        except Exception as e:
+            print(f"Error: {e}")
+            print("Please check your configuration and try again, attempting to reload in 3 seconds...")
+            time.sleep(3)
+            continue
+        
